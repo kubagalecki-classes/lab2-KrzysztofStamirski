ab073ed91b878324e437a1596dd79ae1104bedf4
StamirskiK <01123929@pw.edu.pl>
2020-11-11 21:26:09 +0000

    rev01

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -2,7 +2,39 @@
 
 #include "Resource.hpp"
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
+class ResourceManager{
+  private:
+    Resource res;
+  
+  public:
+   ResourceManager(){
+     this->res = Resource();
+   }
+
+  double get(){
+    return this->res.get();
+  }
+
+    ResourceManager(const ResourceManager&)
+    {
+      this->res = ResourceManager;
+    }
+    ResourceManager(ResourceManager&&) noexcept
+    {
+
+    }
+    ResourceManager& operator=(const ResourceManager&)
+    {
+  
+        return *this;
+    }
+    ResourceManager& operator=(ResourceManager&&) noexcept
+    {
+ 
+        return *this;
+    }
+
+  ~ResourceManager(){
+
+  }
 };
